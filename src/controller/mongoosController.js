3e66efa7110301eb6fb5d1c04bbc9dfcff094995
Demons-alt <fahm3411@gmail.com>
2022-10-13 03:08:14 +0800

    mempurnakan respon

--- src/controller/mongoosController.js
+++ src/controller/mongoosController.js
@@ -4,36 +4,37 @@ const RedisService = require('../module/RedisConnect');
 
 // menampilkan semua data
 const getAllpermis = async(req, res) => {
-    const permis = await Permis.find()
+    
     try {
-        if (!permis) {
+        const permis = await Permis.find()
+        // return BaseRespon.Success(res, permis)
+        if (permis == null) {
             const zero = {
                 message : 'no data to display ',
             }
             return BaseRespon.Success(res, zero)
         }
-        // return BaseRespon.Success(res, permis)
+        try {
+            const dataRedis = await RedisService.get('AllData')
+            if(dataRedis) {
+                console.log('data in redis')
+                // return res.send(JSON.parse(dataRedis))
+                return BaseRespon.Success(res, JSON.parse(dataRedis))
+            }
+    
+            console.log('store data in redis');
+            await RedisService.setEx('AllData',10,JSON.stringify(permis))
+            BaseRespon.Success(res, permis)
+        } catch (error) {
+            console.log(error)
+            return BaseRespon.Failed(res, 'gagal' , error)
+            
+        }
     } catch (error) {
         return BaseRespon.Failed(res, error)
         console.log(error);
     }
-
-    try {
-        const dataRedis = await RedisService.get('AllData')
-        if(dataRedis) {
-            console.log('data in redis')
-            // return res.send(JSON.parse(dataRedis))
-            return BaseRespon.Success(res, JSON.parse(dataRedis))
-        }
-
-        console.log('store data in redis');
-        await RedisService.setEx('AllData',10,JSON.stringify(permis))
-        BaseRespon.Success(res, permis)
-    } catch (error) {
-        console.log(error)
-        return BaseRespon.Failed(res, 'gagal' , error)
-        
-    }
+    
 
     
 
@@ -43,30 +44,34 @@ const getAllpermis = async(req, res) => {
 //get One Data
 const getOnepermis = async(req, res) => {
     const Id = req.params.Idpermis
-    const permis = await Permis.findById(Id)
+    
     try {
         const permis = await Permis.findById(Id)
         // return BaseRespon.Success(res, permis)
+        if (!permis) {
+            return BaseRespon.flexibelRes(res, true, 'not found', 404)
+        }
+        try {
+            const dataRedis = await RedisService.get('OneData')
+            if(dataRedis) {
+                console.log('data in redis')
+                // return res.send(JSON.parse(dataRedis))
+                return BaseRespon.Success(res, JSON.parse(dataRedis))
+            }
+    
+            console.log('store data in redis');
+            await RedisService.setEx('OneData',10,JSON.stringify(permis))
+            BaseRespon.Success(res, permis)
+        } catch (error) {
+            console.log(error)
+            return BaseRespon.Failed(res, 'gagal' , error)
+            
+        }
     } catch (error) {
         return BaseRespon.Failed(res, error)
         
     }
-    try {
-        const dataRedis = await RedisService.get('OneData')
-        if(dataRedis) {
-            console.log('data in redis')
-            // return res.send(JSON.parse(dataRedis))
-            return BaseRespon.Success(res, JSON.parse(dataRedis))
-        }
-
-        console.log('store data in redis');
-        await RedisService.setEx('OneData',10,JSON.stringify(permis))
-        BaseRespon.Success(res, permis)
-    } catch (error) {
-        console.log(error)
-        return BaseRespon.Failed(res, 'gagal' , error)
-        
-    }
+    
 
 
 }
@@ -93,9 +98,10 @@ const updatePermis = async(req, res) => {
         return BaseRespon.Success(res, "notfound")
     }
      try {
-        const updatePermis = await Permis.findOneAndUpdate(Id, Data)
+        const updatePermis = await Permis.findOneAndUpdate(Id, {$set: Data})
+        return BaseRespon.Success(res, updatePermis)
      } catch (error) {
-        console.log(error);
+        return BaseRespon.Failed(res, error)
         
      }
 }
@@ -104,16 +110,13 @@ const updatePermis = async(req, res) => {
 const deletePermis = async(req,res) => {
     const Id = req.params.Idpermis
     const Cek = await Permis.findById(Id)
-
-    const response = {
-        status : 'ga tau'}
-    if (!cek) {
+    if (!Cek) {
         return BaseRespon.Success(res, "not found")
     } try {
         const deletePermis = await Permis.deleteOne({_id : Id})
-        res.json(deletePermis)
+        return BaseRespon.flexibelRes(res,'deleted',deletePermis ,200)
     } catch (error) {
-        console.log(error);
+        return BaseRespon.Failed(res, error)
     }
 }
 
